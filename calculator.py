d52071a3fde6b849aff0bedcbab1a0ad22f18a21
Enemyz0r <rares_taut@yahoo.com>
2017-03-10 12:43:33 +0200

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,45 @@
+def sumation(a, b):
+    """Sum calculation
+
+    Args:
+        a, b - numbers of operation
+
+    Returns:
+        sum of a and b
+    """
+    return a + b
+
+def subtraction(a, b):
+    """Subtraction calculation
+
+    Args:
+        a, b - numbers of operation
+
+    Returns:
+        subtraction of b from a
+    """
+    return a - b
+
+def produce(a, b):
+    """Produce calculation
+
+    Args:
+        a, b - numbers of operation
+
+    Returns:
+        produce of a with b
+    """
+    return a * b
+
+def division(a, b):
+    """Division calculation
+
+    Args:
+        a,b - numbers of operation
+
+    Returns:
+        division of a and b
+    """
+    return a / b
+
+
